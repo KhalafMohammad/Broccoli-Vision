8abc17e7f25fc98a9db6401c4ae34a136d408ee9
mhama <mhama@MSI>
2025-05-14 12:54:32 +0200

    different angle translation

---
+++ anglecalc.py
@@ -0,0 +1,25 @@
+import math as m
+
+y2 = 0
+y1 = 0
+x2 = 3
+x1 = 0
+
+calc_angle = m.atan2 ( y2 - y1 , x2 - x1 )
+
+if ( calc_angle < 0 ): 
+    calc_angle += m.pi * 2
+
+angle = m.degrees ( calc_angle )
+# if(angle < 90 and angle > 0):
+#     angle = 90 - angle
+# elif(angle > 90 and angle < 180):
+#     angle = 90 - angle
+# elif(angle > 180 and angle < 270):
+#     angle = 90 - angle
+# elif(angle > 270 and angle < 360):
+#     angle = 90 - angle
+
+# if angle < 0:
+#     angle = angle + 360
+print( "angle found" , angle )
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-elif contour.CONTOURS_ENABLED:
-				# 	# show contours image
-				# 	src_contours = contour.getContours(frame)
-				# 	cv2.imshow(root_window, src_contours)
